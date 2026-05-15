569721f284e931a7e5b71ef44cea1abf08dc2f1b
callmetango <callmetango@users.noreply.github.com>
2026-05-15 04:13:40 +0000

    Update add-dependencies-repo.sh

--- scripts/add-dependencies-repo.sh
+++ scripts/add-dependencies-repo.sh
@@ -7,16 +7,17 @@ DEPS_PATH="${DEPS_PATH%/}"
 
 REPO_PATH="${HOME}/work/${2:-dependencies}"
 
-DEP_FILES="$(find "$DEPS_PATH" -maxdepth 1 -name '*.pkg.*' -print -quit 2>/dev/null)"
-if [ ! "$DEP_FILES" ]; then
+mkdir -p "$REPO_PATH"
+cd "$REPO_PATH" || exit
+
+find "$DEPS_PATH" -maxdepth 1 -name '*.pkg.* -not -name '*.pkg.*.sig' \
+	-exec sh -c 'for PKG in "$@"; do repo-add dependencies.db.tar "$PKG"; done' - {} +
+
+if [ ! -e depedencies.db.tar ]; then
 	echo "no dependency packages found, skipping"
 	exit 0
 fi
 
-mkdir -p "$REPO_PATH"
-cd "$REPO_PATH" || exit
-
-repo-add dependencies.db.tar "$DEPS_PATH"/*.pkg.{tar,tar.bz2,tar.gz,tar.lrz,tar.lz,tar.lz4,tar.lzo,tar.xz,tar.zst,tar.Z}
 tee -a /etc/pacman.conf <<- EOF
 
 	[dependencies]
